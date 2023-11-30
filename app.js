b30dc57d0a9658ef4aa96b8c866dfe02e7f3738b
abrahamjeron <abrahamjeron40@gmail.com>
2023-11-30 07:10:26 +0530

    changes done

---
+++ app.js
@@ -0,0 +1,28 @@
+const kowsika = document.getElementById("kowsika");
+const naveen = document.getElementById("naveen");
+const thalha = document.getElementById("thalha");
+const niranjan = document.getElementById("niranjan");
+const sriman = document.getElementById("sriman");
+const harini = document.getElementById("harini");
+const abra = document.getElementById("abra")
+kowsika.addEventListener("click",function(){
+    location.href="./kowsika/index.html";
+});
+naveen.addEventListener("click",function(){
+    location.href="./naveen/index.html";
+});
+thalha.addEventListener("click",function(){
+    location.href="./thalha-portfolio-main/page.html";
+});
+niranjan.addEventListener("click",function(){
+    location.href="./niranjan-portfilio-main/index.html";
+});
+sriman.addEventListener("click",function(){
+    location.href="./My Professional Portfolio/sriman.html";
+});
+harini.addEventListener("click",function(){
+    location.href="./harini/index.html";
+});
+abra.addEventListener("click",function(){
+    location.href="./My Professional Portfolio/index.html";
+});
